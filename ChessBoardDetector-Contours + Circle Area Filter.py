21c697d246b4c20f35766e7a1ea244dce35114cb
Alessandro Bordonali <alessandro.bordo41@gmail.com>
2022-10-08 13:25:27 +0200

    trying to dynamically calculate bounding circle radius

--- ChessBoardDetector-Contours + Circle Area Filter.py
+++ ChessBoardDetector-Contours + Circle Area Filter.py
@@ -7,7 +7,7 @@ import cv2
 #cv.imshow('Board', img)
 
 
-capture = cv2.VideoCapture("photos/test.mp4")
+capture = cv2.VideoCapture("photos/Test.mp4")
 
 
 window_name = 'Square Detect'
@@ -19,6 +19,7 @@ max_area = 20000
 
 title_trackbarDelta = 'delta'
 
+
 def on_trackbarMin(val):
     global min_area
     min_area = val
@@ -28,6 +29,7 @@ def on_trackbarMax(val):
     global max_area
     max_area = val
 
+
 def computeImage(frame):
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     se = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 8))
@@ -36,16 +38,19 @@ def computeImage(frame):
     out_binary = cv2.threshold(out_gray, 0, 255, cv2.THRESH_OTSU)[1]
     return out_binary
 
+
 def computeContours(frame):
     cnts = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     return cnts
+
 
 def hasFourVertices(coords):
     if len(coords) != 4:
         return False
     else:
         return True
+
 
 def detectBoardContourMode(image, contours):
     valid_contours = []
@@ -54,9 +59,9 @@ def detectBoardContourMode(image, contours):
 
         if area > min_area and area < max_area:
 
-            approx = cv2.approxPolyDP(c, 0.10 * cv2.arcLength(c, True), True) #al posto di 0.009 ho messo 0.10
+            approx = cv2.approxPolyDP(c, 0.10 * cv2.arcLength(c, True), True)  # al posto di 0.009 ho messo 0.10
 
-            if hasFourVertices(approx): #controllo che ci siano almeno 4 angoli
+            if hasFourVertices(approx):  # controllo che ci siano almeno 4 angoli
                 valid_contours.append(approx)
                 cv2.drawContours(image, [approx], 0, (255, 255, 255), 2)
 
@@ -70,7 +75,47 @@ def detectBoardContourMode(image, contours):
                     pass
     return valid_contours
 
+
+def checkIfPointIsInsideCircle(center, radius, point):
+    # (x - center_x)² + (y - center_y)² < radius² <-- equation
+    dx = point[0] - center[0]
+    dy = point[1] - center[1]
+
+    return dx * dx + dy * dy < radius * radius
+
+
+# Calculate the Euclidean distance
+# between two points
+def distance(x1, y1, x2, y2):
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
+
+
+# Function to calculate K closest points
+def kClosest(points, target, K):
+    pts = []
+    n = len(points)
+    d = []
+
+    for i in range(n):
+        d.append({
+            "first": distance(points[i][0], points[i][1], target[0], target[1]),
+            "second": i
+        })
+
+    d = sorted(d, key=lambda l: l["first"])
+
+    for i in range(K):
+        pt = []
+        pt.append(points[d[i]["second"]][0])
+        pt.append(points[d[i]["second"]][1])
+        pts.append(pt)
+
+    return pts
+
+radius = 0
+pointsInsideCircle = 0
 def applyCircleAreaFilter(image, contours):
+    global radius, pointsInsideCircle
     try:
         valid_points = []
 
@@ -87,21 +132,25 @@ def applyCircleAreaFilter(image, contours):
         medianX = int(sumX / len(valid_points))
         medianY = int(sumY / len(valid_points))
 
-        cv2.circle(image, (medianX, medianY), 200, (255, 255, 255), 2)
+        # for point in valid_points:
+        #     if pointsInsideCircle <= 64:
+        #         while not checkIfPointIsInsideCircle((medianX, medianY), radius, point):
+        #             radius += 10
+        #
+        #         pointsInsideCircle += 1
 
-    except:
-        pass
+        closestPoints = kClosest(valid_points, [medianX, medianY], 256)
 
+        cv2.circle(image, (medianX, medianY), int(distance(medianX, medianY, closestPoints[-1][0], closestPoints[-1][1])), (255, 255, 255), 2)
+    except Exception as e:
+        print(e)
 
 
 cv2.namedWindow(window_name)
 cv2.createTrackbar(title_trackbarMin, window_name, min_area, max_area, on_trackbarMin)
 cv2.createTrackbar(title_trackbarMax, window_name, min_area, max_area, on_trackbarMax)
 
-
-
 while True:
-
 
     isTrue, image = capture.read()
     thresh = computeImage(image)
@@ -114,10 +163,8 @@ while True:
 
     applyCircleAreaFilter(my_img_1, valid_contours)
 
-
     cv2.imshow('thresh', my_img_1)
     cv2.imshow(window_name, image)
-
 
     if cv2.waitKey(20) & 0xFF == ord('d'):
         break
